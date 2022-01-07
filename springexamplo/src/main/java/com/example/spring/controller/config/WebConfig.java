215266160c574b58125ae317d3040903065a5d0d
Felipe Ferro <felipe.nascimento00@edu.pucrs.br>
2022-01-07 02:02:35 -0300

    vscode e intellij

---
+++ springexamplo/src/main/java/com/example/spring/controller/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.example.spring_mvc_mongo_gradle.controller.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    // Serialização é o processo de tradução de estruturas de dados
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+            builder.deserializers(new LocalDateTimeDeserializer(DateTimeFormatter.ISO_LOCAL_DATE));
+        };
+    }
+}
